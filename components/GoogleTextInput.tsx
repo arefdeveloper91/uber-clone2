4e9707455b0f3add7a26a7f533a24af704d7d7a8
Arush Singh <sarushmad@gmail.com>
2024-08-30 12:49:23 +0530

    added Google Text Input

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -2,7 +2,11 @@ import { GoogleInputProps } from "@/types/type";
 import { Text, View } from "react-native"
 
 const GoogleTextInput = ({
-    icon, initialLocation, containerStyle, textInputBackgroundColor, handlePress
+    icon, 
+    initialLocation, 
+    containerStyle, 
+    textInputBackgroundColor, 
+    handlePress,
 }: GoogleInputProps) => (
     <View className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}>
         <Text>Search</Text>
